b3153a75889641b83ddd5721c16fac4a7f84b916
John MacAulay <merjohn@bigpond.net.au>
2021-03-31 13:16:53 +1100

    manual add of pharmacy stock item shows correctly on it's index page

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -7,9 +7,9 @@ class StockItem < ApplicationRecord
       new_stock_item = StockItem.new
       new_stock_item.name = row[0]      
       new_stock_item.scrape_date = row[2]
-      new_stock_item.price_at_scrape = row[3].to_s.delete(".").to_i
+      new_stock_item.price_at_scrape = row[3].to_s.delete('.').to_i
  
-      new_stock_item.price_reduction_rec_retail_at_scrape = row[4].to_s.delete(".").to_i
+      new_stock_item.price_reduction_rec_retail_at_scrape = row[4].to_s.delete('.').to_i
       new_stock_item.save
     end
   end
