446598278da6902a4f3bdae32ddde8d028605b66
John MacAulay <merjohn@bigpond.net.au>
2021-04-19 18:27:25 +1000

    a lot of refactoring in StockItem self.upload

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -5,86 +5,22 @@ class StockItem < ApplicationRecord
 
   def self.upload(file)
     return if file.nil?
-
     CSV.foreach(file.path) do |row|
-      # well I really need to refactor this
-      item_exists = false
-      stock_array = StockItem.all
-      stock_array.each do |item|
-        item_exists = true if item.name == row[0]
-      end
-      if item_exists == false # need to do something about the headers, if I leave them in 
+      if StockItem.find_by(name: row[0]).nil?
         # gosh this is a lot of hassle to avoid using decimals for money!!!
         new_stock_item = StockItem.new
         new_stock_item.name = row[0]
         new_stock_item.scrape_date = row[2]
-
-        dec_index = row[3].index('.')
-        case dec_index
-        when nil
-          new_stock_item.price_at_scrape = "#{row[3]}00"
-        when 1
-          if row[3].to_s.length == 3
-            new_stock_item.price_at_scrape = "#{row[3].to_s.delete('.')}0"
-          else
-            new_stock_item.price_at_scrape = row[3].to_s.delete('.').to_i
-          end
-        else
-          new_stock_item.price_at_scrape = row[3].to_s.delete('.').to_i 
-        end
-
-        dec_rec_index = row[4].index('.')
-        case dec_rec_index
-        when nil
-          new_stock_item.price_reduction_rec_retail_at_scrape = "#{row[4]}00"
-        when 1
-          if row[4].to_s.length == 3
-            new_stock_item.price_reduction_rec_retail_at_scrape = "#{row[4].to_s.delete('.')}0"
-          else
-            new_stock_item.price_reduction_rec_retail_at_scrape = row[4].to_s.delete('.')
-          end
-        else
-          new_stock_item.price_reduction_rec_retail_at_scrape = row[4].to_s.delete('.').to_i
-        end
-
+        new_stock_item.price_at_scrape = price_at_scrape_set(row)
+        new_stock_item.price_reduction_rec_retail_at_scrape = price_reduction_rec_retail_at_scrape_set(row)
         new_stock_item.price_description = row[1]
         new_stock_item.save
-
-      elsif item_exists == true
-        # need to update item.
+      elsif !StockItem.find_by(name: row[0]).nil?
         update_item = StockItem.find_by name: row[0]
         update_item.scrape_date = row[2]
-
-        update_item.price_at_scrape = row[3].to_s.delete('.').to_i
         update_item.price_reduction_rec_retail_at_scrape = row[4].to_s.delete('.').to_i
-        dec_index = row[3].index('.')
-        case dec_index
-        when nil
-          update_item.price_at_scrape = "#{row[3]}00"
-        when 1
-          if row[3].to_s.length == 3
-            update_item.price_at_scrape = "#{row[3].to_s.delete('.')}0"
-          else
-            update_item.price_at_scrape = row[3].to_s.delete('.').to_i
-          end
-        else
-          update_item.price_at_scrape = row[3].to_s.delete('.').to_i
-        end
-
-        dec_rec_index = row[4].index('.')
-        case dec_rec_index
-        when nil
-          update_item.price_reduction_rec_retail_at_scrape = "#{row[4]}00"
-        when 1
-          if row[4].to_s.length == 3
-            update_item.price_reduction_rec_retail_at_scrape = "#{row[4].to_s.delete('.')}0"
-          else
-            update_item.price_reduction_rec_retail_at_scrape = row[4].to_s.delete('.')
-          end
-        else
-          update_item.price_reduction_rec_retail_at_scrape = row[4].to_s.delete('.').to_i
-        end
-
+        update_item.price_at_scrape = price_at_scrape_set(row)
+        update_item.price_reduction_rec_retail_at_scrape = price_reduction_rec_retail_at_scrape_set(row)
         update_item.price_description = row[1]
         update_item.save
       end
@@ -104,5 +40,38 @@ class StockItem < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search_by_name_apn, against: [:name, :apn_barcode_1], using: { tsearch: { prefix: true } }
 
+  private
 
+  def self.price_at_scrape_set(row)  
+    dec_index = row[3].index('.')
+    case dec_index
+    when nil
+      price_at_scrape = "#{row[3]}00"
+    when 1
+      if row[3].to_s.length == 3
+        price_at_scrape = "#{row[3].to_s.delete('.')}0"
+      else
+        price_at_scrape = row[3].to_s.delete('.').to_i
+      end
+    else
+      price_at_scrape = row[3].to_s.delete('.').to_i 
+    end
+    price_at_scrape
+  end
+
+  def self.price_reduction_rec_retail_at_scrape_set(row)
+    dec_rec_index = row[4].index('.')
+    case dec_rec_index
+    when nil
+      price_reduction_rec_retail_at_scrape = "#{row[4]}00"
+    when 1
+      price_reduction_rec_retail_at_scrape = "#{row[4].to_s.delete('.')}0" if row[4].to_s.length == 3    
+      else
+        price_reduction_rec_retail_at_scrape = row[4].to_s.delete('.')
+      end
+    else
+      price_reduction_rec_retail_at_scrape = row[4].to_s.delete('.').to_i
+    end
+    price_reduction_rec_retail_at_scrape
+  end
 end
